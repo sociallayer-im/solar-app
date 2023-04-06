39f7fce50a4b8cb9b66630ea8efd929a3c3200af
qieman <253058016@qq.com>
2023-04-06 18:32:47 +0800

    Update DetailScrollBox.tsx

--- src/components/compose/Detail/atoms/DetailScrollBox/DetailScrollBox.tsx
+++ src/components/compose/Detail/atoms/DetailScrollBox/DetailScrollBox.tsx
@@ -11,33 +11,37 @@ function DetailScrollBox (props: DetailScrollBoxProp) {
     let startScroll:number, touchStart:number, touchCurrent:number
 
     const touchStartCb = (e: any) => {
-        e.stopPropagation();
-        // startScroll = e.target.scrollTop;
-        // touchStart = e.targetTouches[0].pageY;
+        const slide: any = e.currentTarget
+        slide.style.overflow = 'hidden'
     }
 
-    const touchMoveCb = (e: any) => {
-        // touchCurrent = e.targetTouches[0].pageY;
-        // const touchesDiff = touchCurrent - touchStart;
-        // // @ts-ignore
-        // const slide: any = e.currentTarget
-        // const onlyScrolling =
-        //     (slide.scrollHeight > slide.offsetHeight) &&
-        //     (
-        //         (touchesDiff < 0 && startScroll === 0) ||
-        //         (touchesDiff > 0 && startScroll === (slide.scrollHeight - slide.offsetHeight)) ||
-        //         (startScroll > 0 && startScroll < (slide.scrollHeight - slide.offsetHeight))
-        //     );
-        // if (onlyScrolling) {
-        //     e.stopPropagation();
-        // }
-
-        e.stopPropagation();
+    const touchEnd = (e: any) => {
+        const slide: any = e.currentTarget
+        slide.style.overflow = 'auto'
     }
+
+    // const touchMoveCb = (e: any) => {
+    //     touchCurrent = e.targetTouches[0].pageY;
+    //     const touchesDiff = touchCurrent - touchStart;
+    //     // @ts-ignore
+    //     const slide: any = e.currentTarget
+    //     const onlyScrolling =
+    //         (slide.scrollHeight > slide.offsetHeight) &&
+    //         (
+    //             (touchesDiff < 0 && startScroll === 0) ||
+    //             (touchesDiff > 0 && startScroll === (slide.scrollHeight - slide.offsetHeight)) ||
+    //             (startScroll > 0 && startScroll < (slide.scrollHeight - slide.offsetHeight))
+    //         );
+    //     if (onlyScrolling) {
+    //         e.stopPropagation();
+    //     }
+    //
+    //     e.stopPropagation();
+    // }
 
     return <div
         onTouchStart={ touchStartCb }
-        onTouchMove={ touchMoveCb }
+        onTouchEnd={ touchEnd }
         ref={ div } className='detail-scroll-box'>
         { props.children }
     </div>
