ca8661fbb410379b5b20047893c04f03155c496b
qieman <253058016@qq.com>
2023-04-10 20:28:15 +0800

    Update index.tsx

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,20 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 
-const Home = lazy(() => import('../pages/Home'))
-const Profile = lazy(() => import('../pages/Profile/Profile'))
-const Group = lazy(() => import('../pages/Group/Group'))
-const Login = lazy(() => import('../pages/Login'))
-const Regist = lazy(() => import('../pages/Regist'))
-const RegistGroup = lazy(() => import('../pages/RegistGroup'))
-const CreateBadge = lazy(() => import('../pages/CreateBadge'))
-const Issue = lazy(() => import('../pages/Issue'))
-const Invite = lazy(() => import('../pages/Invite'))
-const IssueSuccess = lazy(() => import('../pages/IssueSuccess'))
+
+// package
+import Home from '../pages/Home'
+import Profile from '../pages/Profile/Profile'
+import Group from '../pages/Group/Group'
+import Login from '../pages/Login'
+import Regist from '../pages/Regist'
+import RegistGroup from '../pages/RegistGroup'
+import CreateBadge from '../pages/CreateBadge'
+
+
+// lazy load
+
+// const Home = lazy(() => import('../pages/Home'))
+// const Profile = lazy(() => import('../pages/Profile/Profile'))
+// const Group = lazy(() => import('../pages/Group/Group'))
+// const Login = lazy(() => import('../pages/Login'))
+// const Regist = lazy(() => import('../pages/Regist'))
+// const RegistGroup = lazy(() => import('../pages/RegistGroup'))
+// const CreateBadge = lazy(() => import('../pages/CreateBadge'))
+
 const Search = lazy(() => import('../pages/Search'))
 const Event = lazy(() => import('../pages/Event'))
 const Error = lazy(() => import('../pages/Error'))
-
+const Issue = lazy(() => import('../pages/Issue'))
+const Invite = lazy(() => import('../pages/Invite'))
+const IssueSuccess = lazy(() => import('../pages/IssueSuccess'))
 
 
 function AppRouter () {
