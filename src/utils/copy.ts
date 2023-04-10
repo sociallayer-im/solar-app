8ac350f11e3d021b0fafa84bb08b32d024f56261
qieman <253058016@qq.com>
2023-04-10 23:01:37 +0800

    Update copy.ts

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,7 +1,7 @@
 export default function copy (text: string) {
     const oInput = document.createElement('input')
     oInput.value = text
-    oInput.style.position = 'absolute'
+    oInput.style.position = 'fixed'
     oInput.style.opacity = '0'
     document.body.appendChild(oInput)
     oInput.select()
