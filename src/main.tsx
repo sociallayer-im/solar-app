cab72654dde0b346bee985c1eb43d14c3b683c34
qieman <253058016@qq.com>
2023-04-11 10:50:37 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Buffer } from 'buffer'
-import { TrackJS } from 'trackjs'
 
 window.Buffer = Buffer
-
-TrackJS.install({ token: '903fb1dd5e8c4ca5b12efe83aa5d29a4' })
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
