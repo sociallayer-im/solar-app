950ab17d2ca34a4d24b5794849643ccffe11c45b
qieman <253058016@qq.com>
2023-04-11 11:21:53 +0800

    update

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,9 +1,5 @@
+import copyText from 'copy-to-clipboard'
+
 export default function copy (text: string) {
-    const oInput: any = document.getElementById('CopyInput')
-    oInput!.value = text
-    oInput!.style.position = 'fixed'
-    oInput!.style.opacity = '0'
-    document.body.appendChild(oInput)
-    oInput.select()
-    document.execCommand('Copy')
+    copyText(text)
 }
