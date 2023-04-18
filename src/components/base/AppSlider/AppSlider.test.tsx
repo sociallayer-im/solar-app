d673a5d8c64b5b06b5d0e544f8528703babdd556
qieman <253058016@qq.com>
2023-04-18 13:53:59 +0800

    Update AppSlider.test.tsx

--- src/components/base/AppSlider/AppSlider.test.tsx
+++ src/components/base/AppSlider/AppSlider.test.tsx
@@ -3,7 +3,7 @@ import AppSlider from './AppSlider'
 
 describe('AppSlider', async () => {
     it('正常渲染', () => {
-        render(<AppSlider value={[1]} step={0.1} range={1} />)
+        render(<AppSlider value={[1]} step={0.1} max={1} min={0.1} />)
         expect(screen.getByTestId('AppSlider')).toBeInTheDocument()
     })
 })
