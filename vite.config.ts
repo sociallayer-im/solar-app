3f37a1bc8e00e7ca96e73019f85567506b98b89e
qieman <253058016@qq.com>
2023-05-08 16:32:04 +0800

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+let options: UserConfigExport = {
     plugins: [react(), visualizer()],
     test: {
         globals: true,
@@ -38,11 +38,38 @@ export default defineConfig({
             {
                 find: 'bn.js',
                 replacement:'node_modules/bn.js/lib/bn.js'
-            },
-            {
-                find: 'buffer',
-                replacement:'node_modules/buffer/index.js'
             }
         ]
     }
-})
+}
+const env = process.env.NODE_ENV
+const resolve = {
+    alias:[
+        {
+            find: 'wagmi/dist',
+            replacement:'wagmi/dist'
+        },
+        {
+            find: './dist/connectors/walletConnect',
+            replacement:'wagmi/dist/connectors/walletConnect.js'
+        },
+        {
+            find: 'bn.js',
+            replacement:'node_modules/bn.js/lib/bn.js'
+        }
+    ]
+}
+
+/**
+ * 不知道为啥在开发模式下修改Buffer的依赖路径会报错，
+ * 所以只能在构建的时候加上
+ */
+if (env === 'production') {
+    resolve.alias.push({
+        find: 'buffer',
+        replacement:'node_modules/buffer/index.js'
+    })
+}
+
+options.resolve = resolve
+export default defineConfig(options)
